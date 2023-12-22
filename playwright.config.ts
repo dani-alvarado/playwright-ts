c6dd224e4721bb312047a5c4338a5aa47e8e4d17
Daniel Alvarado <djalvaradog@unal.edu.co>
2023-12-22 13:26:12 -0500

    added dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import { config as dotenvConfig } from "dotenv";
 
 /**
  * Read environment variables from file.
@@ -6,6 +7,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+dotenvConfig();
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://automationexercise.com/",
+    baseURL: process.env.UI_BASE_URL || "https://automationexercise.com/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
